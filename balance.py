90737d8ab7fdb10a459795e6468aa7adbf521c39
rsiwin <rsiwin@gmail.com>
2019-07-30 12:22:37 +0900

    일정시간 Balance변동이 없을 경우 이메일로 안내기능 추가

--- balance.py
+++ balance.py
@@ -4,41 +4,63 @@ import threading
 import smtplib
 from email.mime.text import MIMEText
 
+url = "http://api.etherscan.io/api?module=account&action=balance&address="
+hd_address = "0xcf398a8363fDa31eF003CcbBb9Ff66f9533F279f"  #hd_balance
+my_address = "0x7E7a5181FA32F99De6938a8537e3F95Ec69fAfCF"  #my_balance
+kb_address = "0xb9954de4d4d49dfc5856dcaca8f70d6939e2e407"  #kb_balance
+messages = ''
+
+
 class BalanceCheckTask:
+    CurrentBalance = 0
+    DiffBalance = 0
     def __init__(self):
         pass
 
-    def SendEmail(self):
-        # e-Mail Server Setting
-        smtp = smtplib.SMTP('smtp.gmail.com', 587)
-        smtp.ehlo()      # say Hello
-        smtp.starttls()  # TLS 사용
-        smtp.login('kildongra@gmail.com', 'NjdoC6%9&2')
-
-        # Balance Check URL Setting
+    def BalanceChk(self,Address):
+        global CurrentBalance
         url = "http://api.etherscan.io/api?module=account&action=balance&address="
-        #hd_address = "0xcf398a8363fDa31eF003CcbBb9Ff66f9533F279f"
-        kb_address = "0xb9954de4d4d49dfc5856dcaca8f70d6939e2e407"
-        #hd_balance = round(int(requests.get(url + hd_address).json().get("result")) * 0.000000000000000001,4)
-        kb_balance = round(int(requests.get(url + kb_address).json().get("result")) * 0.000000000000000001,4)
+        CurrentBalance = round(int(requests.get(url + Address).json().get("result")) * 0.000000000000000001,4)
 
-        #if hd_balance < 1 :
-        #    msg = MIMEText('')
-        #    msg['Subject'] = '[warning] HD wallet balance is ' + str(hd_balance)
-        #    smtp.sendmail('kildongra@gmail.com', 'rsi@coinplug.com', msg.as_string())
-        #    print("Send e-Mail HDCard Balance = " + str(hd_balance))
-
-        if kb_balance < 1 :
-            msg = MIMEText('')
-            msg['Subject'] = '[warning] KB wallet balance is ' + str(kb_balance)
-            smtp.sendmail('kildongra@gmail.com', 'rsi@coinplug.com', msg.as_string())
-            print("Send e-Mail KBCard Balance = " + str(kb_balance))
-
+    def AnotherPeriodBalanceChk(self,Address):
+        global DiffBalance
+        url = "http://api.etherscan.io/api?module=account&action=balance&address="
+        DiffBalance = round(int(requests.get(url + Address).json().get("result")) * 0.000000000000000001,4)
+    def send_mail(self,messages):
+        # e-Mail Server Setting
+        global smtp
+        text = messages
+        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('kildongra@gmail.com', 'NjdoC6%9&2')
+        message = MIMEText(text)
+        message['Subject'] = 'Anchoring service report'
+        message['From'] = 'kildongra@gmail.com'
+        message['To'] = 'rsi@coinplug.com'
+        smtp.sendmail('kildongra@gmail.com', 'rsi@coinplug.com', message.as_string())
         smtp.quit()
 
-        threading.Timer(3600,self.SendEmail).start()
+    def Balance(self):
+        self.BalanceChk(kb_address)
+        #print("KBCard Current Balance is " + str(CurrentBalance))
+        if CurrentBalance < 1 :
+            messages = "KBCard current wallet balance is " + str(CurrentBalance)
+            self.send_mail(messages)
+            print("KBCard current wallet balance is " + str(CurrentBalance))
 
+        threading.Timer(60,self.Balance).start()
 
+    def KeepAlive(self):
+
+        # BeforeKbBalance = CurrentBalance
+        if DiffBalance == CurrentBalance :
+            messages = 'Check anchoring service!! \n current balance is ' + str(CurrentBalance)
+            self.send_mail(messages)
+            print('Check anchoring service!! \n current balance is ' + str(CurrentBalance))
+        self.AnotherPeriodBalanceChk(kb_address)
+
+        threading.Timer(120,self.KeepAlive).start()
     # def TaskB(self):
     #     print ("Process B")
     #     threading.Timer(3, self.TaskB).start()
@@ -46,8 +68,13 @@ class BalanceCheckTask:
 def main():
     print ('Anchoring Wallet Balance Chech Function')
     at = BalanceCheckTask()
-    at.SendEmail()
+    at.BalanceChk(kb_address)
+    at.AnotherPeriodBalanceChk(kb_address)
+    at.send_mail(messages)
+    at.Balance()
+    at.KeepAlive()
     # at.TaskB()
 
 if __name__ == '__main__':
     main()
+
