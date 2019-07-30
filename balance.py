3eb29641ddc99791748ef68d954c735ad0ff0973
rsiwin <rsiwin@gmail.com>
2019-07-30 13:16:14 +0900

    안내 이메일 대신 텔레그램으로 메시지 전송기능 변경

--- balance.py
+++ balance.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 import requests
 import threading
-import smtplib
-from email.mime.text import MIMEText
 
-url = "http://api.etherscan.io/api?module=account&action=balance&address="
+url = "https://api.etherscan.io/api?module=account&action=balance&address="
 hd_address = "0xcf398a8363fDa31eF003CcbBb9Ff66f9533F279f"  #hd_balance
 my_address = "0x7E7a5181FA32F99De6938a8537e3F95Ec69fAfCF"  #my_balance
 kb_address = "0xb9954de4d4d49dfc5856dcaca8f70d6939e2e407"  #kb_balance
-messages = ''
+chain_word = "&tag=latest&apikey="
+api_key = "P8T6BGCW24W1JKIYB4VVYNHB3D3MUQ1FVI"
+messages = 'Now Start!!'
+tele_url = "https://api.telegram.org/bot863557793:AAE6wt7bTQMBI9YmsaThFydMf2ChQBB5IIY/sendMessage"
 
 
 class BalanceCheckTask:
@@ -19,48 +20,39 @@ class BalanceCheckTask:
 
     def BalanceChk(self,Address):
         global CurrentBalance
-        url = "http://api.etherscan.io/api?module=account&action=balance&address="
-        CurrentBalance = round(int(requests.get(url + Address).json().get("result")) * 0.000000000000000001,4)
+        CurrentBalance = round(int(requests.get(url + Address + chain_word + api_key).json().get("result")) * 0.000000000000000001,4)
 
     def AnotherPeriodBalanceChk(self,Address):
         global DiffBalance
-        url = "http://api.etherscan.io/api?module=account&action=balance&address="
-        DiffBalance = round(int(requests.get(url + Address).json().get("result")) * 0.000000000000000001,4)
-    def send_mail(self,messages):
-        # e-Mail Server Setting
-        global smtp
+        DiffBalance = round(int(requests.get(url + Address + chain_word + api_key).json().get("result")) * 0.000000000000000001,4)
+
+    def send_teleMsg(self,messages):
         text = messages
-        smtp = smtplib.SMTP('smtp.gmail.com', 587)
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.login('kildongra@gmail.com', 'NjdoC6%9&2')
-        message = MIMEText(text)
-        message['Subject'] = 'Anchoring service report'
-        message['From'] = 'kildongra@gmail.com'
-        message['To'] = 'rsi@coinplug.com'
-        smtp.sendmail('kildongra@gmail.com', 'rsi@coinplug.com', message.as_string())
-        smtp.quit()
+        params = {'chat_id': '-374074806', 'text': messages}
+        res = requests.get(tele_url, params=params)
+
 
     def Balance(self):
         self.BalanceChk(kb_address)
         #print("KBCard Current Balance is " + str(CurrentBalance))
         if CurrentBalance < 1 :
             messages = "KBCard current wallet balance is " + str(CurrentBalance)
-            self.send_mail(messages)
+            self.send_teleMsg(messages)
             print("KBCard current wallet balance is " + str(CurrentBalance))
 
-        threading.Timer(60,self.Balance).start()
+        threading.Timer(3600,self.Balance).start()
 
     def KeepAlive(self):
 
         # BeforeKbBalance = CurrentBalance
         if DiffBalance == CurrentBalance :
             messages = 'Check anchoring service!! \n current balance is ' + str(CurrentBalance)
-            self.send_mail(messages)
+            self.send_teleMsg(messages)
             print('Check anchoring service!! \n current balance is ' + str(CurrentBalance))
         self.AnotherPeriodBalanceChk(kb_address)
 
-        threading.Timer(120,self.KeepAlive).start()
+        threading.Timer(7200,self.KeepAlive).start()
+
     # def TaskB(self):
     #     print ("Process B")
     #     threading.Timer(3, self.TaskB).start()
@@ -70,7 +62,7 @@ def main():
     at = BalanceCheckTask()
     at.BalanceChk(kb_address)
     at.AnotherPeriodBalanceChk(kb_address)
-    at.send_mail(messages)
+    at.send_teleMsg(messages)
     at.Balance()
     at.KeepAlive()
     # at.TaskB()
