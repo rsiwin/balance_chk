2b47e23c5a9a82960c7d239c6693baf917a11877
rsiwin <rsiwin@gmail.com>
2019-07-30 13:21:31 +0900

    버그패치(구동중 다운되는현상, List갯수가 5개 미만일 경우에는 문자 보내지 않도록수정)

--- balance.py
+++ balance.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 import requests
 import threading
+import time
 
-url = "https://api.etherscan.io/api?module=account&action=balance&address="
+now = time.localtime()
+url = "api.etherscan.io/api?module=account&action=balance&address="
 hd_address = "0xcf398a8363fDa31eF003CcbBb9Ff66f9533F279f"  #hd_balance
 my_address = "0x7E7a5181FA32F99De6938a8537e3F95Ec69fAfCF"  #my_balance
 kb_address = "0xb9954de4d4d49dfc5856dcaca8f70d6939e2e407"  #kb_balance
 chain_word = "&tag=latest&apikey="
 api_key = "P8T6BGCW24W1JKIYB4VVYNHB3D3MUQ1FVI"
 messages = 'Now Start!!'
-tele_url = "https://api.telegram.org/bot863557793:AAE6wt7bTQMBI9YmsaThFydMf2ChQBB5IIY/sendMessage"
+tele_url = "api.telegram.org/bot863557793:AAE6wt7bTQMBI9YmsaThFydMf2ChQBB5IIY/sendMessage"
 BalanceInList = list()
+CheckPeriod = 3600 # unit = sec
+CountInList = 5
+LowBalance = 0.5
+
+def CurTime():
+    global DisCurTime
+    DisCurTime = "[%04d-%02d-%02d %02d:%02d:%02d]" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+    return (DisCurTime)
 
 class BalanceCheckTask:
     CurrentBalance = 0
@@ -19,33 +29,46 @@ class BalanceCheckTask:
 
     def BalanceChk(self,Address):
         global CurrentBalance
-        CurrentBalance = round(int(requests.get(url + Address + chain_word + api_key).json().get("result")) * 0.000000000000000001,4)
+        CurrentBalance = round(int(requests.get(b'https://' + url + Address + chain_word + api_key).json().get('result')) * 0.000000000000000001,4)
         global BalanceInList
-        BalanceInList.insert(0,CurrentBalance)
+        # BalanceInList.insert(0,CurrentBalance)
+        if len(BalanceInList) == 5 :
+                BalanceInList.pop() # List내마지막 요소 삭제
 
     def send_teleMsg(self,messages):
         text = messages
         params = {'chat_id': '-374074806', 'text': messages}
-        res = requests.get(tele_url, params=params)
+        res = requests.get(b'https://' + tele_url, params=params)
 
     def Balance(self):
         self.BalanceChk(kb_address)
 
-        if CurrentBalance < 0.5 :
+        if len(BalanceInList) < CountInList-1 and CurrentBalance > LowBalance:
+            BalanceInList.insert(0,CurrentBalance)
+        elif len(BalanceInList) < CountInList-1 and CurrentBalance < LowBalance:
             messages = "KBCard current wallet balance is too low : " + str(CurrentBalance)
             self.send_teleMsg(messages)
-            print("KBCard current wallet balance is too low " + str(CurrentBalance))
-
-
-        if BalanceInList[0] == BalanceInList[-1] :
-            messages = 'Check anchoring service!! \n current balance is ' + str(CurrentBalance)
+            print(str(CurTime()) + '  KBCard current wallet balance is too low ' + str(CurrentBalance))
+            BalanceInList.insert(0,CurrentBalance)
+        elif len(BalanceInList) >= CountInList-1 and CurrentBalance > LowBalance:
+            BalanceInList.insert(0,CurrentBalance)
+            BalanceInList.pop()
+            if BalanceInList[0] == BalanceInList[-1] :
+                messages = 'Check anchoring service!! \n current balance is ' + str(CurrentBalance)
+                self.send_teleMsg(messages)
+                print(str(CurTime()) +'  Check anchoring service!! \n current balance is ' + str(CurrentBalance))
+        else:
+            messages = "KBCard current wallet balance is too low : " + str(CurrentBalance)
             self.send_teleMsg(messages)
-            print('Check anchoring service!! \n current balance is ' + str(CurrentBalance))
+            print(str(CurTime()) +'  KBCard current wallet balance is too low ' + str(CurrentBalance))
+            BalanceInList.insert(0,CurrentBalance)
+            BalanceInList.pop()
+            if BalanceInList[0] == BalanceInList[-1] :
+                messages = 'Check anchoring service!! \n current balance is ' + str(CurrentBalance)
+                self.send_teleMsg(messages)
+                print(str(CurTime()) +'  Check anchoring service!! \n current balance is ' + str(CurrentBalance))
 
-            if len(BalanceInList) == 5 :   #List내 설정 갯수만큼만 유지
-                BalanceInList.pop()
-
-        threading.Timer(3600,self.Balance).start()   #매 주기마다 Balance Check
+        threading.Timer(CheckPeriod,self.Balance).start()   #매 주기마다 Balance Check
 
 def main():
     print ('Anchoring Wallet Balance Chech Function')
